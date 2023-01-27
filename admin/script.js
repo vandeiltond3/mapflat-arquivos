5b56ebdecb1067bb4a1dd83d79c09645bb436e83
vandeiltond3 <vandeiltond3@gmail.com>
2023-01-27 08:18:08 -0300

    tela admin criada

---
+++ admin/script.js
@@ -0,0 +1,22 @@
+tog = document.querySelector('.material-symbols-outlined')
+menu = document.querySelector('.menu')
+
+/*function clique(){
+    if(menu.style.display == "block"){
+        menu.style.display = "none"
+        tog.innerText = "menu"
+    }else{
+        menu.style.display = "block"
+        tog.innerText = "close"
+    }
+}*/
+
+let show = true;
+
+function clique(){
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menu.classList.toggle('on', show)
+    show = !show;
+}
