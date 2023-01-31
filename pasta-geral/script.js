843bf775297d8dd699ac017013325cf66f1cbf22
vandeiltond3 <vandeiltond3@gmail.com>
2023-01-31 17:51:08 -0300

    padronizado

--- pasta-geral/script.js
+++ pasta-geral/script.js
@@ -1,35 +1,4 @@
-tog = document.querySelector('.material-symbols-outlined')
-menu = document.querySelector('.menu-addimvel')
 
-/*function clique(){
-    if(menu.style.display == "block"){
-        menu.style.display = "none"
-        tog.innerText = "menu"
-    }else{
-        menu.style.display = "block"
-        tog.innerText = "close"
-    }
-}*/
-
-let show = true;
-
-function cliques(){
-
-    document.body.style.overflow = show ? "hidden" : "initial"
-
-    menu.classList.toggle('on', show)
-    show = !show;
-
-    if(tog.innerText == "menu"){
-        tog.innerText = "close"
-    }else{
-        tog.innerText = "menu";
-    }
-}
-
-
-
-//demais menus
 
 togs = document.querySelector('.material-symbols-outlined')
 menus = document.querySelector('.menu-adm')
