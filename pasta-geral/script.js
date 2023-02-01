b9468e29c6ceeda6ea942d5e84be9cd8f051a1c0
vandeiltond3 <vandeiltond3@gmail.com>
2023-02-01 18:36:31 -0300

    api de estado e cidade feito

--- pasta-geral/script.js
+++ pasta-geral/script.js
@@ -18,4 +18,37 @@ function clique(){
     }else{
         togs.innerText = "menu";
     }
-}
+};
+
+
+// estados e cidades
+
+
+const urlUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+const cidade = document.querySelector('#cidade')
+const uf = document.querySelector('#uf')
+
+uf.addEventListener('change', async function(){
+    const urlCidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+uf.value+'/municipios'
+    const request = await fetch(urlCidades)
+    const response = await request.json()
+
+    let options = ''
+    response.forEach(function(cidades){
+    options += '<option>'+cidades.nome+'</option>'
+    })
+    cidade.innerHTML = options
+})    
+
+window.addEventListener('load', async ()=>{
+    const request = await fetch(urlUF)
+    const response = await request.json()
+
+    const options = document.createElement('optgroup')
+    options.setAttribute('label', 'UFs')
+    response.forEach(function(uf){
+        options.innerHTML += '<option>'+uf.sigla+'</option>'
+    })
+
+    uf.append(options)
+})
